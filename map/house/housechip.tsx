ccde470e77d53ba6bb404106cf36cb4b9f9757df
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-01-17 21:35:46 +0900

    house chip test

---
+++ map/house/housechip.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="housechip" tilewidth="64" tileheight="64" tilecount="6" columns="6">
+ <image source="../../dot/housechip.png" width="384" height="64"/>
+</tileset>
---
+++ map/test/tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tree" tilewidth="64" tileheight="96" tilecount="1" columns="1">
+ <image source="dot/export/tree.png" width="64" height="96"/>
+</tileset>
