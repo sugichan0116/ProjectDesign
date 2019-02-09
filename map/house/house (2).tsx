1f6e2cca69204bd85221c3c91ea6a58f6e5c59f4
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-02-09 16:10:26 +0900

    house

---
+++ map/house/house (2).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="house (2)" tilewidth="64" tileheight="32" tilecount="8" columns="2">
+ <image source="../../dot/export/house (2).png" width="128" height="128"/>
+</tileset>
---
+++ map/terra/girl2.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="girl2" tilewidth="192" tileheight="192" tilecount="4" columns="0">
+ <grid orientation="isometric" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="96" source="../../dot/export/tree.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../dot/export/girl2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+ <tile id="5">
+  <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+</tileset>
