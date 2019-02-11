0df2451d5dc2ff89c97dab19e994f3c2627b6402
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-02-11 22:54:13 +0900

    emotion

---
+++ map/emotion.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="emotion" tilewidth="64" tileheight="64" tilecount="8" columns="4">
+ <image source="../dot/export/emotion.png" width="256" height="128"/>
+</tileset>
