583915137d202e662923e206996e622869625ab8
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-02-12 12:28:13 +0900

    house

--- map/terra/girl2.tsx
+++ map/terra/girl2.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="girl2" tilewidth="192" tileheight="192" tilecount="4" columns="0">
+<tileset version="1.2" tiledversion="1.2.1" name="girl2" tilewidth="192" tileheight="192" tilecount="13" columns="0">
  <grid orientation="isometric" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="96" source="../../dot/export/tree.png"/>
@@ -12,5 +12,32 @@
  </tile>
  <tile id="5">
   <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+ <tile id="6">
+  <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+ <tile id="7">
+  <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+ <tile id="8">
+  <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+ <tile id="9">
+  <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+ <tile id="10">
+  <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+ <tile id="11">
+  <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+ <tile id="12">
+  <image width="192" height="192" source="../../dot/house_minka.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../../dot/export/girl.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../../dot/export/girl3.png"/>
  </tile>
 </tileset>
---
+++ map/terra/obj.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="obj" tilewidth="64" tileheight="96" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="96" source="../../dot/export/box (3).png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="96" source="../../dot/export/bush (2).png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../dot/export/fence (3).png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../../dot/export/grass (11).png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../dot/export/grass (12).png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../dot/export/grass (13).png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../dot/export/grass (14).png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../../dot/export/grass.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../../dot/export/item_rock (2).png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../../dot/export/item_rock (3).png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../../dot/export/item_rock (4).png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../../dot/export/item_rock.png"/>
+ </tile>
+</tileset>
