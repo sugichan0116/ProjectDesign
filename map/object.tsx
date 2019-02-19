29983728e6a68f86127059c2e5a46658a1645ab6
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-02-19 20:22:24 +0900

    RGB shading

--- map/object.tsx
+++ map/object.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="object" tilewidth="256" tileheight="256" tilecount="3" columns="0">
+<tileset version="1.2" tiledversion="1.2.2" name="object" tilewidth="256" tileheight="256" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="../dot/export/tree.png"/>
@@ -9,5 +9,8 @@
  </tile>
  <tile id="2">
   <image width="224" height="160" source="../dot/export/boat.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="../dot/export/boat (4).png"/>
  </tile>
 </tileset>
