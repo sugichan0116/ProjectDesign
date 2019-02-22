71e39f990c7c72198287085b310e484e2909fedf
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-02-22 18:54:14 +0900

    pyramid

--- map/material.tsx
+++ map/material.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="material" tilewidth="64" tileheight="128" tilecount="25" columns="0">
+<tileset version="1.2" tiledversion="1.2.2" name="material" tilewidth="64" tileheight="128" tilecount="26" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="../dot/export/weed.png"/>
@@ -75,5 +75,8 @@
  </tile>
  <tile id="27">
   <image width="64" height="64" source="../dot/export/dead-tree.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="../dot/export/footprint.png"/>
  </tile>
 </tileset>
--- map/object.tsx
+++ map/object.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="object" tilewidth="256" tileheight="256" tilecount="5" columns="0">
+<tileset version="1.2" tiledversion="1.2.2" name="object" tilewidth="512" tileheight="320" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="../dot/export/tree.png"/>
@@ -15,5 +15,8 @@
  </tile>
  <tile id="7">
   <image width="128" height="128" source="../dot/export/rock-plate.png"/>
+ </tile>
+ <tile id="8">
+  <image width="512" height="320" source="../dot/export/pyramid.png"/>
  </tile>
 </tileset>
