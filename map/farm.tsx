899c154296224408ffd393d9d996e3da07d6e5f1
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-02-23 21:11:22 +0900

    farm

--- map/farm.tsx
+++ map/farm.tsx
@@ -1,4 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="farm" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="../dot/export/map/farm.png" width="64" height="64"/>
+<tileset version="1.2" tiledversion="1.2.1" name="farm" tilewidth="64" tileheight="64" tilecount="20" columns="4">
+ <image source="../dot/export/map/farm.png" width="256" height="320"/>
+ <terraintypes>
+  <terrain name="farm" tile="0"/>
+ </terraintypes>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain=",0,0,0"/>
+ <tile id="3" terrain="0,,0,0"/>
+ <tile id="4" terrain="0,0,0,"/>
+ <tile id="5" terrain="0,0,,0"/>
+ <tile id="6" terrain=",0,,"/>
+ <tile id="7" terrain="0,,,"/>
+ <tile id="8" terrain=",,0,"/>
+ <tile id="9" terrain=",,,0"/>
+ <tile id="10" terrain="0,0,,"/>
+ <tile id="11" terrain=",0,,0"/>
+ <tile id="12" terrain=",,0,0"/>
+ <tile id="13" terrain="0,,0,"/>
 </tileset>
