17639655ac7424223285bd1bbb8127ee2183ed83
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-02-25 14:28:21 +0900

    shrimp

--- map/item.tsx
+++ map/item.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="item" tilewidth="32" tileheight="32" tilecount="6" columns="0">
+<tileset version="1.2" tiledversion="1.2.1" name="item" tilewidth="32" tileheight="32" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="../dot/export/item/yuri-book.png"/>
@@ -18,5 +18,8 @@
  </tile>
  <tile id="5">
   <image width="32" height="32" source="../dot/export/item/spinach.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../dot/export/shrimp.png"/>
  </tile>
 </tileset>
