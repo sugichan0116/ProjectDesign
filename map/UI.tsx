47eba4cb4c5c00d5be018bdf0a9ec42402b5d260
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-02-25 22:00:59 +0900

    UI

--- map/UI.tsx
+++ map/UI.tsx
@@ -1,7 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="UI" tilewidth="128" tileheight="48" tilecount="1" columns="0">
+<tileset version="1.2" tiledversion="1.2.1" name="UI" tilewidth="384" tileheight="128" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="128" height="48" source="../dot/export/system/message.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../dot/export/system/item-slot.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../dot/export/system/item-slot-sample.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../dot/export/system/ui-shading.png"/>
+ </tile>
+ <tile id="4">
+  <image width="384" height="64" source="../dot/export/system/menu-icons.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../dot/export/system/clock.png"/>
  </tile>
 </tileset>
