a6c91e188ce1307aa0f365788f84de732680fd5d
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-02-25 14:49:43 +0900

    message

---
+++ map/UI.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="UI" tilewidth="128" tileheight="48" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="48" source="../dot/export/system/message.png"/>
+ </tile>
+</tileset>
